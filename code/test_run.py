f275ada7c63172976128ff6e7ec5057243a1ac8b
HyoKyuKang <djs080307@gmail.com>
2025-12-02 18:03:45 +0900

    fix: Fix RL env

--- code/test_run.py
+++ code/test_run.py
@@ -1,17 +1,6 @@
 from connexion_env import ConnexionEnv
-
 env = ConnexionEnv()
 obs, info = env.reset()
-print("obs shape:", obs.shape)
-print("action_size:", env.action_size)
-
-for i in range(10):
-    mask = env.get_valid_action_mask()
-    # 유효한 액션 아무거나 하나 골라봄
-    import numpy as np
-    valid_actions = np.where(mask)[0]
-    a = int(valid_actions[0])
-    obs, r, done, trunc, info = env.step(a)
-    print(i, "reward:", r, "done:", done)
-    if done:
-        obs, info = env.reset()
+print(obs.shape, env.action_size)
+mask = env.get_valid_action_mask()
+print(mask.sum(), "valid actions")
